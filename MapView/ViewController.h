5d3c7aeecf9c52efbf7d26251dd220b577c3d15b
wangyifei <wangyifei@ycw-wangrui3.tech.bitauto.com>
2012-10-15 10:20:11 +0800

    Initial Commit

---
+++ MapView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapView
+//
+//  Created by wangyifei on 12-10-15.
+//  Copyright (c) 2012年 wangyifei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MapViewTests/MapViewTests.h
@@ -0,0 +1,13 @@
+//
+//  MapViewTests.h
+//  MapViewTests
+//
+//  Created by wangyifei on 12-10-15.
+//  Copyright (c) 2012年 wangyifei. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MapViewTests : SenTestCase
+
+@end
